45b9a91c724f840981a78c57813f42a7cb4cbb7e
Kiril Daskalov <k.daskalov@magicsolutions.bg>
2018-07-24 09:37:23 +0300

    - folder restructure

---
+++ Task 2 - CSS layout/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const devMode = process.env.NODE_ENV !== 'production'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+module.exports = {
+  entry: {
+    'main': './src/index.js',
+    'accordion.kendoUI': './src/js/accordion.kendoUI.js'
+  },
+  resolve: {
+    extensions: ['.js', '.css', '.scss', '.sass']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['es2015'] }
+        }
+      },
+      {
+        // css / sass / scss loader for webpack
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? 'accordion.kendoUI.css' : 'accordion.kendoUI.[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new LiveReloadPlugin(),
+    require('autoprefixer')
+  ],
+};
