1cdbc6720455405e09b090d8cc81b38dcc9d0113
Kiril Daskalov <k.daskalov@magicsolutions.bg>
2018-08-01 04:17:19 +0300

    - reconfig webpack

--- Task 1 - Accordion widget/src/index.js
+++ Task 1 - Accordion widget/src/index.js
@@ -1,1 +1,3 @@
-require('./css/style.scss');
+require('./assets/css/style.scss');
+require('./assets/accordion/css/accordion.kendoUI.scss');
+require('./assets/accordion/js/accordion.kendoUI.js');
--- Task 1 - Accordion widget/webpack.config.js
+++ Task 1 - Accordion widget/webpack.config.js
@@ -1,18 +1,78 @@
+// const path = require('path');
+// const devMode = process.env.NODE_ENV !== 'production'
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+// module.exports = {
+//   entry: {
+//     'main': './src/index.js',
+//     'accordion.kendoUI': './src/js/accordion.kendoUI.js'
+//   },
+//   resolve: {
+//     extensions: ['.js', '.css', '.scss', '.sass']
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: { presets: ['es2015'] }
+//         }
+//       },
+//       {
+//         // css / sass / scss loader for webpack
+//         test: /\.(css|sass|scss)$/,
+//         use: [
+//           MiniCssExtractPlugin.loader,
+//           'css-loader',
+//           'postcss-loader',
+//           'sass-loader',
+//         ]
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin({
+//       // Options similar to the same options in webpackOptions.output
+//       // both options are optional
+//       filename: devMode ? 'accordion.kendoUI.css' : 'accordion.kendoUI.[hash].css',
+//       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+//     }),
+//     new LiveReloadPlugin(),
+//     require('autoprefixer')
+//   ],
+// };
+
 const path = require('path');
 const devMode = process.env.NODE_ENV !== 'production'
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    'main': './src/index.js',
-    'accordion.kendoUI': './src/js/accordion.kendoUI.js'
+    'main': './src/index.js'
   },
   resolve: {
     extensions: ['.js', '.css', '.scss', '.sass']
   },
+  devServer: {
+    compress: true,
+    port: 3333
+  },
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            interpolate: true
+          }
+        }],
+      },
       {
         test: /\.js$/,
         use: {
@@ -21,25 +81,40 @@ module.exports = {
         }
       },
       {
-        // css / sass / scss loader for webpack
         test: /\.(css|sass|scss)$/,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              config: {
+                path: './postcss.config.js'
+              },
+              plugins: loader => [require('autoprefixer')()]
+            }
+          },
+          { loader: 'sass-loader', options: { sourceMap: true } }
         ]
       }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: devMode ? 'accordion.kendoUI.css' : 'accordion.kendoUI.[hash].css',
+      filename: 'assets/css/style.css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     }),
-    new LiveReloadPlugin(),
-    require('autoprefixer')
+    new LiveReloadPlugin({
+      appendScriptTag: true
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets/img', to: 'assets/img' },
+      { from: 'src/assets/svg', to: 'assets/svg' }
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
   ],
 };
+
