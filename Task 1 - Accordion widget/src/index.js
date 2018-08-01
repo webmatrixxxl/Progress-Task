b3fcdce092f8f1877f83c3128e0889ea4d62ab11
Kiril Daskalov <k.daskalov@magicsolutions.bg>
2018-08-01 04:42:19 +0300

    - add trigger animated arrows - some code cleanup - add meaningful content

--- Task 1 - Accordion widget/src/index.js
+++ Task 1 - Accordion widget/src/index.js
@@ -1,3 +1,3 @@
-require('./assets/css/style.scss');
 require('./assets/accordion/css/accordion.kendoUI.scss');
 require('./assets/accordion/js/accordion.kendoUI.js');
+require('./assets/css/style.scss');
