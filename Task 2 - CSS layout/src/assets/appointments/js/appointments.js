a41e816e690d59ad49d05b3a28d87678cdb288a6
Kiril Daskalov <k.daskalov@magicsolutions.bg>
2018-08-01 03:49:47 +0300

    - add timer check for element with id

--- Task 2 - CSS layout/src/assets/appointments/js/appointments.js
+++ Task 2 - CSS layout/src/assets/appointments/js/appointments.js
@@ -1,6 +1,11 @@
 const clockEl = document.getElementById('appointments__time-id');
 
 function startTime() {
+  if (!clockEl) {
+    console.log('Use "appointments__time-id" as ID to start clock');
+    return;
+  }
+
   const d = new Date();
   const amOrPm = (d.getHours() < 12) ? 'AM' : 'PM';
   const hour = (d.getHours() < 12) ? d.getHours() : d.getHours() - 12;
